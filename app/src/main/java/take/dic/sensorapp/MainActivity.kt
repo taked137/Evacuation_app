e824cc11d72778207a7661dc6c27a7cabcdd231c
Takeya Daichi <takeya@c0de-net.local>
2019-07-09 19:20:30 +0900

    整理

--- app/src/main/java/take/dic/sensorapp/MainActivity.kt
+++ app/src/main/java/take/dic/sensorapp/MainActivity.kt
@@ -11,51 +11,12 @@ import org.altbeacon.beacon.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity(), BeaconConsumer {
-
-
-    private lateinit var listView: ListView
-    private lateinit var arrayAdapter: ArrayAdapter<String>
-    private lateinit var realm: Realm
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
-
-        //listView = findViewById(R.id.listview)
-        //arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
-        //listView.adapter = arrayAdapter
-
-        Realm.init(this)
-    }
-
-    /*
-    override fun onResume() {
-        super.onResume()
-        beaconManager.bind(this)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        beaconManager.unbind(this)
-    }
-    */
-
-    override fun onBeaconServiceConnect() {
-
-    }
-
-    // 試験用(削除予定)
-    override fun onStop() {
-
-        realm = Realm.getDefaultInstance()
-        realm.beginTransaction()
-        realm.delete(BeaconModel::class.java)
-        realm.commitTransaction()
-        realm.close()
-
-        super.onStop()
     }
 }
--- app/src/main/java/take/dic/sensorapp/ValueFragment.kt
+++ app/src/main/java/take/dic/sensorapp/ValueFragment.kt
@@ -1,6 +1,5 @@
 package take.dic.sensorapp
 
-import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
@@ -11,6 +10,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.realm.Realm
 import org.altbeacon.beacon.*
 import take.dic.sensorapp.databinding.FragmentValueBinding
 import java.text.SimpleDateFormat
@@ -19,18 +19,31 @@ import java.util.*
 class ValueFragment : Fragment(), BeaconConsumer {
     private lateinit var state: String
     private lateinit var mRegion: Region
-    private val beaconList = mutableListOf<BeaconModel>()
-    private val data = mutableListOf<String>()
+    private lateinit var realm: Realm
 
     //iBeacon認識のためのフォーマット設定
     private val IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
 
     private lateinit var beaconManager: BeaconManager
 
+    val gps = GPSValue(title = "GPS", latitude = "緯度", longitude = "経度")
+    val mBeacon = BeaconValue()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentValueBinding.inflate(inflater, container, false)
+        binding.gps = gps
+
+        beaconManager = BeaconManager.getInstanceForApplication(activity!!)
+        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
+        binding.beacon = mBeacon
+
+        Realm.init(activity!!)
+        return binding.root
+    }
+
     override fun getApplicationContext(): Context = activity!!.applicationContext
 
     override fun unbindService(p0: ServiceConnection?) {
-        //beaconManager.unbind(this)
         activity!!.unbindService(p0)
     }
 
@@ -46,26 +59,33 @@ class ValueFragment : Fragment(), BeaconConsumer {
         beaconManager.unbind(this)
     }
 
+    //TODO: 試験用(削除予定)
+    override fun onStop() {
+        realm = Realm.getDefaultInstance()
+        realm.beginTransaction()
+        realm.delete(BeaconModel::class.java)
+        realm.commitTransaction()
+        realm.close()
+
+        super.onStop()
+    }
+
     override fun onBeaconServiceConnect() {
         mRegion= Region("iBeacon", null, null, null)
-        Log.e("hhh","epe")
         beaconManager.addMonitorNotifier(object : MonitorNotifier {
             override fun didEnterRegion(region: Region) {
                 //領域への入場を検知
                 state = "Enter Region"
-                Log.e("hsh","$state")
             }
 
             override fun didExitRegion(region: Region) {
                 //領域からの退場を検知
                 state = "Exit Region"
-                Log.e("hsh","$state")
             }
 
             override fun didDetermineStateForRegion(i: Int, region: Region) {
                 //領域への入退場のステータス変化を検知
                 state = "Determine State$i"
-                Log.e("hsh","$state")
             }
         })
         try {
@@ -76,24 +96,20 @@ class ValueFragment : Fragment(), BeaconConsumer {
         }
 
         beaconManager.addRangeNotifier { beacons, _ ->
-            //検出したBeaconの情報を全て出力
             for (beacon in beacons) {
-                Log.e("fff",beacon.toString())
-                //realm = Realm.getDefaultInstance()
+                realm = Realm.getDefaultInstance()
                 var id: Int = try {
-                    //realm.where(BeaconModel::class.java).findAll().size + 1
-                    1
+                    realm.where(BeaconModel::class.java).findAll().size + 1
                 } catch (e: Exception) {
                     1
                 }
                 val currentBeacon = BeaconModel(
                     id,
-                    beacon.id1.toString(),
                     beacon.id2.toString(),
+                    beacon.id3.toString(),
                     beacon.rssi,
                     SimpleDateFormat("yyyy.MM.dd HH:mm:ss z").format(Calendar.getInstance().time)
                 )
-                /*
                 realm.executeTransaction {
                     val model = realm.createObject(BeaconModel::class.java!!, id)
                     model.major = currentBeacon.major
@@ -103,43 +119,19 @@ class ValueFragment : Fragment(), BeaconConsumer {
                     realm.copyToRealm(model)
                 }
                 realm.where(BeaconModel::class.java).findAll().forEach { Log.e("got", it.toString()) }
-                */
 
-                data.add(
+
+                mBeacon.list.add(
                     0,
                     "${currentBeacon.receivedTime}\nmajor: ${currentBeacon.major}, minor: ${currentBeacon.minor}, rssi: ${currentBeacon.rssi}"
                 )
-                //realm.close()
+                realm.close()
             }
-            Log.e("hhh", data.toString())
         }
         try {
             beaconManager.startRangingBeaconsInRegion(mRegion)
         } catch (e: RemoteException) {
             e.printStackTrace()
         }
-
-    }
-
-    val gps = GPSValue(title = "GPS", latitude = "緯度", longitude = "経度")
-    val beacon = BeaconValue()
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentValueBinding.inflate(inflater, container, false)
-        binding.gps = gps
-
-        regist()
-        binding.beacon = beacon
-        return binding.root
-    }
-
-    private fun regist() {
-        beaconManager = BeaconManager.getInstanceForApplication(activity!!)
-        try{
-            beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
-        }catch(e: Exception){
-            Log.e("eeee",e.toString())
-        }
-        beaconManager.bind(this)
     }
 }
