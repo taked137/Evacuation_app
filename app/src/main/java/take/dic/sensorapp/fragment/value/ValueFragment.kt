a945af4a2f1d671d7633e1baaf8ae438f32245bf
Takeya Daichi <takeya@c0de-net.local>
2019-08-19 01:48:54 +0900

    整理

--- app/src/main/java/take/dic/sensorapp/fragment/value/ValueFragment.kt
+++ app/src/main/java/take/dic/sensorapp/fragment/value/ValueFragment.kt
@@ -1,36 +1,21 @@
 package take.dic.sensorapp.fragment.value
 
-import android.app.AlertDialog
-import android.bluetooth.BluetoothAdapter
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Bundle
-import android.os.RemoteException
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import io.realm.Realm
-import org.altbeacon.beacon.*
 import take.dic.sensorapp.R
 import take.dic.sensorapp.fragment.value.acceleration.AccelerationSensorFragment
-import take.dic.sensorapp.fragment.value.beacon.BeaconModel
-import take.dic.sensorapp.fragment.value.beacon.BeaconValue
-import take.dic.sensorapp.databinding.FragmentValueBinding
 import take.dic.sensorapp.fragment.value.beacon.BeaconFragment
 import take.dic.sensorapp.fragment.value.gps.GPSFragment
 import take.dic.sensorapp.fragment.value.gyro.GyroSensorFragment
 import take.dic.sensorapp.fragment.value.orientation.OrientationFragment
-import java.text.SimpleDateFormat
-import java.util.*
 
 class ValueFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        val binding = FragmentValueBinding.inflate(inflater, container, false)
 
         val transaction = activity!!.supportFragmentManager.beginTransaction()
         transaction.add(R.id.gps_container, GPSFragment())
@@ -40,6 +25,6 @@ class ValueFragment : Fragment() {
         transaction.add(R.id.beacon_container, BeaconFragment())
         transaction.commit()
 
-        return binding.root
+        return inflater.inflate(R.layout.fragment_value, container, false);
     }
 }
