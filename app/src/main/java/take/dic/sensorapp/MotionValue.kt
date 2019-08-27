c2073949ea6e09b6bbd763a68ea1b556afadb206
ogawa <tarosan2020@gmail.com>
2019-08-27 09:50:26 +0900

    MotionValueの構造を変える可能性があります

--- app/src/main/java/take/dic/sensorapp/MotionValue.kt
+++ app/src/main/java/take/dic/sensorapp/MotionValue.kt
@@ -1,10 +1,20 @@
 package take.dic.sensorapp
 
+import take.dic.sensorapp.Acceleration.AccelerationValue
+import take.dic.sensorapp.angular.AngularValue
+import take.dic.sensorapp.orientation.OrientationValue
+
 
 class MotionValue {
 
     //motions[0]は加速度、[1]は角速度、[2]は方位
     var motions : MutableList<Motion?> = mutableListOf(Motion("加速度"), Motion("角速度"), Motion("方位"))
+
+    //motionsの代わりにこっちを使うかも
+    /*var acceleration = AccelerationValue("")
+    var angular = AngularValue("")
+    var orient = OrientationValue("")*/
+
 
     //センサーが存在する加速度・角速度・方位情報について、すべてが新しい値を取得していたら更新
     fun updateData() {
